c3c8d84a48eacf13e5c402fe7e9230c08e603f56
Zahan Malkani <zahanm@fb.com>
2020-07-27 16:04:56 -0700

    switch from yargs to commander for async commands

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import * as path from "path";
 
 import * as sqlite3 from "sqlite3";
 import { open } from "sqlite";
-import * as yargs from "yargs";
+import { program } from "commander";
 
 import { BEAR_DB } from "./lib/constants";
 import findDuplicates from "./lib/findDuplicates";
@@ -17,16 +17,18 @@ async function main() {
     mode: sqlite3.OPEN_READONLY,
   });
   try {
-    yargs
-      .scriptName("bear-tracks")
-      .usage("$0 <cmd> [args]")
-      .command("duplicates", "Find duplicate note titles", async function (
-        _argv: yargs.Arguments
-      ) {
+    program.name("bear-tracks");
+
+    program
+      .command("duplicates")
+      .description("Find notes with duplicate titles")
+      .action(async function () {
         const titles = await findDuplicates(db);
         console.log("Notes with duplicated titles:");
         console.log(titles);
-      }).argv;
+      });
+
+    await program.parseAsync();
   } finally {
     await db.close();
   }
