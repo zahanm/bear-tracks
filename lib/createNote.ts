9b3f9d1cdf64060dc4771b3b6b83e5282f6cf775
Zahan Malkani <zahanm@fb.com>
2020-08-16 03:13:40 -0700

    Fix the weekly note to have the right dates

--- lib/createNote.ts
+++ lib/createNote.ts
@@ -12,6 +12,9 @@ export async function createDailyNote(
   const body = `## Plan
 
 ## Done`;
+  if (opts.debug) {
+    console.error(`Create: ${title}`);
+  }
   await bearApiCreateNote(opts, {
     title,
     text: body,
@@ -26,8 +29,9 @@ export async function createDailyNote(
 export async function createWeeklyNote(
   opts: Record<string, any>
 ): Promise<Note> {
-  const end_of_week = moment().add(6, "days");
-  const title = `Plan for ${moment().format("MMM D")} - ${end_of_week.format(
+  const start = moment().add(1, "week").startOf("week");
+  const end = moment().add(1, "week").endOf("week");
+  const title = `Plan for ${start.format("MMM D")} - ${end.format(
     "MMM D, YYYY"
   )}`;
   const body = `## Work
@@ -39,6 +43,9 @@ export async function createWeeklyNote(
 ## Social
 
 ## Daily`;
+  if (opts.debug) {
+    console.error(`Create: ${title}`);
+  }
   await bearApiCreateNote(opts, {
     title,
     text: body,
